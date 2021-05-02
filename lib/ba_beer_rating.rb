539cf74123c93e326e8f7658b019c09e4589dcdf
ericalittle <eteney90@gmail.com>
2021-05-02 15:54:39 -0400

    File structure and Gem created

---
+++ lib/ba_beer_rating.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ba_beer_rating/version"
+
+module BaBeerRating
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ba_beer_rating/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BaBeerRating
+  VERSION = "0.1.0"
+end
